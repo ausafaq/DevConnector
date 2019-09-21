c5c17fe376b0bf7cde0b8495827a3d74da169643
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2019-09-21 16:11:17 -0700

    Added 'Edit Profile' button

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -23,6 +23,13 @@ const Profile = ({
           <Link to='/profiles' className='btn btn-light'>
             Back to profiles
           </Link>
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to='/edit-profile' className='btn btn-dark'>
+                Edit Profile
+              </Link>
+            )}
         </Fragment>
       )}
     </Fragment>
